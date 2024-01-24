81d92762d41ea4b2f3501308feaee01ae2083a96
Otávio Francisco <otaviohbf2002@gmail.com>
2024-01-24 20:40:56 -0300

    Feat(Homepage): search and user page routes added to server.js

--- homepage/server.js
+++ homepage/server.js
@@ -20,6 +20,15 @@ server.get('/feed', () =>{
     return 'PAGINA INICIAL:\nEscutas Recentes:\nFor you:'
 })
 
+server.get('/feed/search', () =>{
+    return 'PAGINA DE PESQUISA:\nPesquise aqui sua musica ->'
+})
+
+//userpage precisa de um parametro para identificar qual eh o usuario em questao
+server.get('/feed/userpage', () =>{
+    return 'PAGINA DO USUARIO:\nAqui uma abobrinha sobre você ->'
+})
+
 server.listen({
     port: 3333,
 })
