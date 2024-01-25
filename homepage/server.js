d0fb4c79dae52587a3e6bb215c47760abcdac8bb
Otávio Francisco <otaviohbf2002@gmail.com>
2024-01-24 21:06:11 -0300

    Fix(Homepage): userpage route fixed. Parameter added to route

--- homepage/server.js
+++ homepage/server.js
@@ -15,18 +15,21 @@ import {fastify} from 'fastify'
 
 const server = fastify()
 
-
+//homepage
 server.get('/feed', () =>{
     return 'PAGINA INICIAL:\nEscutas Recentes:\nFor you:'
 })
 
+//search bar
 server.get('/feed/search', () =>{
     return 'PAGINA DE PESQUISA:\nPesquise aqui sua musica ->'
 })
 
 //userpage precisa de um parametro para identificar qual eh o usuario em questao
-server.get('/feed/userpage', () =>{
-    return 'PAGINA DO USUARIO:\nAqui uma abobrinha sobre você ->'
+server.get('/feed/userpage/:userid', (request, reply) =>{
+    const userId = request.params.userid;
+    console.log(userId)
+    return reply.send('PAGINA DO USUARIO:\nAqui uma abobrinha sobre você ->')
 })
 
 server.listen({
