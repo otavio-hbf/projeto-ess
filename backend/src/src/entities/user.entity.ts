30794f8ab594629b1b159ca66991ab3ce762fd7e
aricneto <aricneto@gmail.com>
2024-02-02 19:08:06 -0300

    feat: add history tracking field to user model

--- backend/src/src/entities/user.entity.ts
+++ backend/src/src/entities/user.entity.ts
@@ -1,14 +1,21 @@
 import BaseEntity from "./base.entity";
+import { IsString, IsBoolean } from "class-validator";
 
 export default class UserEntity extends BaseEntity {
+  @IsString()
   name: string;
+  @IsString()
   email: string;
+  @IsString()
   password: string;
+  @IsBoolean()
+  history_tracking: boolean;
 
   constructor(data: UserEntity) {
     super(data.id || "");
     this.name = data.name;
     this.email = data.email;
     this.password = data.password;
+    this.history_tracking = data.history_tracking;
   }
 }
--- backend/src/src/models/user.model.ts
+++ backend/src/src/models/user.model.ts
@@ -3,10 +3,12 @@ import BaseModel from "./base.model";
 export default class UserModel extends BaseModel {
   name: string;
   email: string;
+  history_tracking: boolean;
 
   constructor(data: UserModel) {
     super(data.id || "");
     this.name = data.name;
     this.email = data.email;
+    this.history_tracking = data.history_tracking;
   }
 }
