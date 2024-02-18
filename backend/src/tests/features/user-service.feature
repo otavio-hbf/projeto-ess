f9d5c2626c5d26f5377c9991a6b90ffc6689e0da
Luan <luaneustaquio52@gmail.com>
2024-02-18 16:44:59 -0300

    fix: fix user service scenarios

--- backend/src/tests/features/user-service.feature
+++ backend/src/tests/features/user-service.feature
@@ -1,2 +1,19 @@
 Feature: user services
 
+Scenario: obter usuário pelo email
+    Given o método createUser foi chamado com o name "samanto" email "samanto@gmail.com" e senha "samanto123"
+    When o método getUserByEmail é chamado com o email "samanto@gmail.com"
+    Then o usuário retornado tem nome "samanto"
+
+Scenario: obter todos os usuários
+    Given existe um usuário de nome "matue" cadastrado
+    When o método getUsers é chamado
+    Then o usuário de nome "matue" deve está na lista de usuários retornada
+
+/*
+Scenario: excluir usuário
+    Given o método createUser foi chamado com o name "samanto" email "samanto@gmail.com" e senha "samanto123"
+    When o método deleteUserWithEmailPassword é chamado com o email "samanto@gmail.com" e senha "samanto123"
+    And o método getUserByEmail é chamado com o email "samanto@gmail.com"
+    Then é retornada a mensagem "user_not_found"
+*/
--- backend/src/tests/services/user.service.steps.ts
+++ backend/src/tests/services/user.service.steps.ts
@@ -39,76 +39,17 @@ defineFeature(feature, (test) => {
     afterEach(() => {
         jest.resetAllMocks();
     });
-
+    
     test('Registration successful', ({ given, when, then }) => {
         given('the system does not have an account with the email “ze@gmail.com” registered', () => {
-            
-        });
-
-        when(/^a "(.*)" request was sent to "(.*)" with the request body being a JSON with name "(.*)" email "(.*)" and password "(.*)"$/, (arg0, arg1, arg2, arg3, arg4) => {
-        });
-
-        then(/^the system registers the new email account “ze@gmail.com” and password “ze(\d+)”$/, (arg0) => {
-
-        });
-    });
-
-    test('Login successful', ({ given, when, then, and }) => {
-        given(/^the system has the account with email “ze@gmail.com” and password “ze(\d+)” registered$/, (arg0) => {
 
         });
 
-        when(/^a GET request was sent to "(.*)" with the request body being a JSON with email "(.*)" and password "(.*)"$/, (arg0, arg1, arg2) => {
+        when(/^a POST request is sent to "(.*)" with the request body being a JSON with name "(.*)" email "(.*)" and password "(.*)"$/, (arg0, arg1, arg2, arg3) => {
 
         });
 
         then(/^the response status should be "(.*)"$/, (arg0) => {
-
-        });
-
-        and(/^the user with email "(.*)" is logged in$/, (arg0) => {
-
-        });
-    });
-
-    test('Delete user', ({ given, when, and, then }) => {
-        given(/^the system has the account with email “ze@gmail.com” and password “ze(\d+)” registered$/, (arg0) => {
-
-        });
-
-        when(/^the system receives a request to delete the account with email "(.*)" and password "(.*)"$/, (arg0, arg1) => {
-
-        });
-
-        and(/^the system deletes the email account "(.*)"$/, (arg0) => {
-
-        });
-
-        then(/^the system does not have the email account "(.*)" registered$/, (arg0) => {
-
-        });
-    });
-
-    test('Update user', ({ given, when, then }) => {
-        given(/^the system has the account with email “ze@gmail.com” and password “ze(\d+)” registered$/, (arg0) => {
-
-        });
-
-        when(/^a PUT request is sent to "(.*)" with the request body being a JSON with email "(.*)" and password "(.*)"$/, (arg0, arg1, arg2) => {
-
-        });
-
-        then(/^the response status should be "(.*)"$/, (arg0) => {
-
-        });
-    });
-
-    test('Unsuccessful registration', ({ given, when, then }) => {
-        given('the system has an account with the email “ze@gmail.com” registered', () => {
-
-        });
-
-        when(/^a POST request was sent to "(.*)" with the request body being a JSON with name "(.*)" email "(.*)" and password "(.*)"$/, (arg0, arg1, arg2, arg3) => { 
 
         });
 
@@ -117,21 +58,4 @@ defineFeature(feature, (test) => {
         });
     });
 
-    test('Unsuccessful login', ({ given, when, then, and }) => {
-        given(/^the system has the account with email “ze@gmail.com” and password “ze(\d+)” registered$/, (arg0) => {
-
-        });
-
-        when(/^a GET request was sent to "(.*)" with the request body being a JSON with email "(.*)" and password "(.*)"$/, (arg0, arg1, arg2) => {
-
-        });
-
-        then(/^the response status should be "(.*)"$/, (arg0) => {
-
-        });
-
-        and(/^the user with email "(.*)" is logged in$/, (arg0) => {
-
-        });
-    });
 });
