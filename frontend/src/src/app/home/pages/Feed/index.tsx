bff8a015c3e9ab061bb9eaac61f979aa145241d0
Otávio Francisco <otaviohbf2002@gmail.com>
2024-03-05 10:24:35 -0300

    prettier formatation applied

--- frontend/src/src/app/home/pages/Feed/index.tsx
+++ frontend/src/src/app/home/pages/Feed/index.tsx
@@ -19,14 +19,13 @@ const Feed = () => {
       spacing={2}
       className={styles.container}
     >
-    <h1>Músicas</h1>
+      <h1>Músicas</h1>
       <div className={styles.listContainer}>
         {state.getSongsRequestStatus.maybeMap({
           loading: () => <span>Carregando...</span>,
           failed: () => <span>Erro ao carregar as músicas!</span>,
           succeeded: (songs) => (
             <>
-              
               <ul className={styles.songList}>
                 {songs.map((song) => (
                   <li key={song.id} className={styles.songListItem}>
@@ -34,17 +33,14 @@ const Feed = () => {
                   </li>
                 ))}
               </ul>
-              
             </>
           ),
         })}
       </div>
-      
+
       <h1>Para você</h1>
-      <div className={styles.listContainer}>
-        
-      </div>
-      
+      <div className={styles.listContainer}></div>
+
       <br />
     </Stack>
   );
