e5923c65b10ce5190bc52a66e22588c648c80a14
Thiago-Ramalho <trm4@cin.ufpe.br>
2024-03-10 22:14:14 -0300

    feat(ContributorsModal): add a button to add a random contributor

--- frontend/src/src/app/home/components/ContributorsModal/index.tsx
+++ frontend/src/src/app/home/components/ContributorsModal/index.tsx
@@ -1,8 +1,9 @@
-import { Modal, ModalClose, Sheet, Typography } from "@mui/joy";
+import { Modal, ModalClose, Sheet, Typography, Button, Alert } from "@mui/joy";
 import PlaylistModel from "../../models/PlaylistModel";
 import UserModel from "../../models/UserModel";
 import { useContext, useEffect, useState } from "react";
 import { PlaylistContext } from "../../context/PlaylistContext";
+import Cookies from "universal-cookie";
 
 interface ContributorsModalProps {
   open: boolean;
@@ -14,6 +15,9 @@ const ContributorsModal = (props: ContributorsModalProps) => {
   const { open, setOpen, playlist } = props;
   const { service } = useContext(PlaylistContext);
   const [contributorsData, setContributorsData] = useState<UserModel[]>([]);
+  const cookies = new Cookies();
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  
 
   useEffect(() => {
     const fetchContributorsData = async () => {
@@ -31,47 +35,74 @@ const ContributorsModal = (props: ContributorsModalProps) => {
     fetchContributorsData();
   }, [open, playlist.contributors, service]);
 
-  return (
-    <Modal
-      aria-labelledby="modal-title"
-      aria-describedby="modal-desc"
-      open={open}
-      onClose={() => setOpen(false)}
-      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-    >
-      <Sheet
-        variant="outlined"
-        sx={{
-          maxWidth: 500,
-          minWidth: 400,
-          borderRadius: "md",
-          p: 3,
-          boxShadow: "lg",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <ModalClose variant="plain" sx={{ m: 1 }} />
-        <Typography
-          component="h2"
-          id="modal-title"
-          level="h3"
-          textColor="inherit"
-          fontWeight="lg"
-          mb={4}
-          sx={{ display: "flex", justifyContent: "center" }}
-        >
-          Contributors:
-        </Typography>
+  const handleAddFakeContributor = (evt) => {
+    let randomContributorId: string;
+    randomContributorId = ((evt.clientX % 3) + 1).toString();
+    const userId = cookies.get("userId");
 
-        {contributorsData.map((contributor, index) => (
-          <Typography key={index} sx={{ mb: 1 }}>
-            {contributor.name}
+    if (playlist.contributors.includes(randomContributorId)) {
+      setErrorMessage("The user is already a contributor");
+    } else if (cookies.get("userId").toString() === randomContributorId){
+      setErrorMessage("The owner can't add itself as contributor");
+    } else {
+      service.addContributorToPlaylist(playlist.id, randomContributorId, userId.toString());
+      setErrorMessage("");
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{
+            maxWidth: 500,
+            minWidth: 400,
+            borderRadius: "md",
+            p: 3,
+            boxShadow: "lg",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <ModalClose variant="plain" sx={{ m: 1 }} />
+          <Typography
+            component="h2"
+            id="modal-title"
+            level="h3"
+            textColor="inherit"
+            fontWeight="lg"
+            mb={4}
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            Contributors:
           </Typography>
-        ))}
-      </Sheet>
-    </Modal>
+
+          {contributorsData.map((contributor, index) => (
+            <Typography key={index} sx={{ mb: 1 }}>
+              {contributor.name}
+            </Typography>
+          ))}
+          {errorMessage && <Alert>{errorMessage}</Alert>}
+          {playlist.createdBy !== cookies.get("userId").toString() ? 
+            null : (<Button
+              onClick={handleAddFakeContributor}
+              variant="outlined"
+              color="primary"
+              data-cy="confirm-rename-playlist"
+            >
+              +Add random contributor
+            </Button>)}
+        </Sheet>
+      </Modal>
+    </>
   );
 };
 
